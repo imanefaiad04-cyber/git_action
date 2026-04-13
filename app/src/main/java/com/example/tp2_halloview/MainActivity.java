057e58b00ec5ab44f31a9d805e5c371312322763
= <=>
2026-04-13 16:08:16 +0100

    Mon premier commit

---
+++ app/src/main/java/com/example/tp2_halloview/MainActivity.java
@@ -0,0 +1,42 @@
+package com.example.tp2_halloview;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class MainActivity extends AppCompatActivity {
+
+    private EditText txt_nom;
+    private TextView txt_aff;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main); //
+
+        txt_nom = findViewById(R.id.id_Enom);
+        txt_aff = findViewById(R.id.id_aff);
+
+    }
+
+
+    public void onClick(View view) {
+
+        if(txt_nom.getText().length() == 0){
+            txt_aff.setText(R.string.msg_vide);
+        }else {
+            String nom = txt_nom.getText().toString();
+            String ress = getString(R.string.re)+ " " + nom;
+            txt_aff.setText(ress);
+
+        }
+
+    }
+}
